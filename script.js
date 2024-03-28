82e85d865656e0e6cb991dc19f03b5c7f3200e48
Singh Pritesh <singhpritesh7599@gmail.com>
2024-03-28 14:12:05 +0530

    update

---
+++ script.js
@@ -0,0 +1,36 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = 'AM';
+
+    if(h==0){
+        h = 12; //12 hour format
+    }
+
+    if(h>12){
+        h = h-12; //12hour format
+        session = 'PM';
+    }
+
+    // if(h<10){
+    //     h = '0' + h;
+    // }
+    // if(m<10){
+    //     m = '0' + m;
+    // }
+    // if(s<10){
+    //     s = '0' + s;
+    // }
+
+    h = (h<10)?'0'+h:h;
+    m = (m<10)?'0'+m:m;
+    s = (s<10)?'0'+s:s;
+
+    document.getElementById("digitalclock").innerHTML = h + ':' + m + ':' + s + session;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
